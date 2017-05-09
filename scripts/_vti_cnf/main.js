5178d4fa1eafe821c9bc115022534bb36547ae22
yzhao12345 <yzhao12345@gmail.com>
2017-05-09 15:10:32 -0400

    Add files via upload

---
+++ scripts/_vti_cnf/main.js
@@ -0,0 +1,10 @@
+vti_encoding:SR|utf8-nl
+vti_author:SR|BUSINESS\\yaozhao
+vti_modifiedby:SR|BUSINESS\\yaozhao
+vti_timelastmodified:TR|09 May 2017 04:59:59 -0000
+vti_timecreated:TR|08 May 2017 05:14:01 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|index.html
+vti_nexttolasttimemodified:TW|09 May 2017 04:57:30 -0000
+vti_cacheddtm:TX|09 May 2017 04:59:59 -0000
+vti_filesize:IR|1010
---
+++ scripts/main.js
@@ -0,0 +1,38 @@
+﻿//var myHeading = document.querySelector('h1');
+//myHeading.textContent = 'Hello world!';
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+  var myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'SCAL is cool, ' + myName;
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.textContent = 'SCAL is cool, ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
+
+/*document.querySelector('h1').onclick = function() {
+    alert('Ouch! Stop poking me!');
+}*/
+
+/*var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/kids.jpg') {
+      myImage.setAttribute ('src','images/kids2.jpg');
+    } else {
+      myImage.setAttribute ('src','images/kids.jpg');
+    }
+}
+*/
